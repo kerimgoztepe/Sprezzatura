43d97f36d4b5a178b26be035ec6180a5842732fa
Kerim Goztepe <kerimgoztepe@yahoo.com>
2020-05-08 00:53:22 +0300

    i added new Hello.java

---
+++ src/main/java/MySampleJavaCodes/Hello1.java
@@ -0,0 +1,10 @@
+package MySampleJavaCodes;
+
+public class Hello1 {
+    public static void main(String[] args) {
+        String name = "John Berger";
+        System.out.println(name);
+        System.out.println(name);
+
+    }
+}

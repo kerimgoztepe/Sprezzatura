ea8aa766d4fb977897d2e6ef45726ec197460749
Kerim Goztepe <kerimgoztepe@yahoo.com>
2020-05-08 00:50:22 +0300

    i added new Constructor.java

---
+++ src/main/java/MySampleJavaCodes/Constructor.java
@@ -0,0 +1,24 @@
+package MySampleJavaCodes;
+
+public class Constructor {
+
+        String name = "John";
+        int age = 30;
+        // below const. example
+        Constructor(){
+            this.name="John Berger";
+            this.age=30;
+        }
+
+        public static void main(String[] args) {
+            Constructor obj1 = new Constructor();
+            Constructor obj2= new Constructor();
+
+            System.out.println("name is = "+obj1.name+"| age is "+obj1.age);
+
+
+        }
+
+
+    }
+

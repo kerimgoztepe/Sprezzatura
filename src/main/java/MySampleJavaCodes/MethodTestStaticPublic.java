a95142afb95da7ff9edaa2fceb59f8166af06ebc
Kerim Goztepe <kerimgoztepe@yahoo.com>
2022-07-27 19:46:48 +0100

    updates

--- src/main/java/MySampleJavaCodes/MethodTestStaticPublic.java
+++ src/main/java/MySampleJavaCodes/MethodTestStaticPublic.java
@@ -2,16 +2,18 @@ package MySampleJavaCodes;
 
 public class MethodTestStaticPublic {
     public void myNewMethod() {
+        //this is a method
         System.out.println("Hello man");
     }
 
     static void myNewMethod2 (){
+        //this is a static method
         System.out.println("Caio Ragazzo");
     }
 
     public static void main(String[] args) {
         MethodTestStaticPublic newTest = new MethodTestStaticPublic();
-        newTest.myNewMethod();
-        myNewMethod2();
+        newTest.myNewMethod(); //you have to create ob=bject to run method
+        myNewMethod2();        //for static method, you don't need object
     }
 }
